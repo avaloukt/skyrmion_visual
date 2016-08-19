5188124f52b5b895804f4ebcab3cbe69e49016b2
Alkistis Valouktsi <avaloukt@gmail.com>
2016-08-19 10:48:06 +0100

    d3 selection filter by attributes value

--- script.js
+++ script.js
@@ -28,19 +28,49 @@ rect.style("fill", "red");
 
  var center_element = $('#0');
  //console.log(center_element.attr('x'), center_element.attr('y'))
-
+ 
+ //Select 3x3 array elements around center
  for (var i = 0; i < all_elements.length; i++) {
-
   if ( parseInt ($(all_elements[i]).attr('x')) < (parseInt(center_element.attr('x'))+26) &&
        parseInt ($(all_elements[i]).attr('x')) > (parseInt(center_element.attr('x'))-26) && 
        parseInt ($(all_elements[i]).attr('y')) < (parseInt(center_element.attr('y'))+26) &&
        parseInt ($(all_elements[i]).attr('y')) > (parseInt(center_element.attr('y'))-26)
      ) { 
 	  	rect_selection.push(all_elements[i]); 
-
-	  	console.log( $(all_elements[i]).attr('id') )
+	  	//console.log( $(all_elements[i]).attr('id') )
 	  	} 
  }
+
+ var subselection = d3.selectAll("rect").filter(function(d){ return d3.select(this).attr('x') >= 75; })
+ subselection.style("fill", "blue"); 
+
+ // // Filter selected rects by their binded data values
+ //    svg.selectAll("rect")
+ //        .data(data)
+ //      //.enter().append("circle")
+ //      .filter(function(d) { return d.close < 400 })
+ //        .style("fill", "red")
+ //        .attr("r", 3.5)
+ //        .attr("cx", function(d) { return x(d.date); })
+ //        .attr("cy", function(d) { return y(d.close); });
+
+ //Bind alter height data
+ // rect_selection.data([2, 2, 2,
+ //                      2, 1, 2,
+ //                      2, 2, 2,]);
+
+ // rect_selection.attr('height', function(d) { return d; });
+
+
+
+
+
+
+
+
+
+
+
 
 });
     
